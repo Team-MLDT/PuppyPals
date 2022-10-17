bcde1fe3bfb55c1395560f40c821b6d946ebca45
Darius <Darius.owens.j@gmail.com>
2022-10-17 10:16:38 -0700

    created project activities

---
+++ app/src/main/java/com/mldt/puppypals/Activities/EditProfile.java
@@ -0,0 +1,16 @@
+package com.mldt.puppypals.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mldt.puppypals.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
--- app/src/main/java/com/mldt/puppypals/Activities/Verify.java
+++ app/src/main/java/com/mldt/puppypals/Activities/Verify.java
@@ -1,14 +1,16 @@
-package com.mldt.puppypals;
+package com.mldt.puppypals.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mldt.puppypals.R;
+
+public class Verify extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_verify);
     }
 }
