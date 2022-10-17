d02f57dec1e89130fe6929af2af7f67c050f74b5
DariusO92 <99936580+DariusO92@users.noreply.github.com>
2022-10-17 10:30:17 -0700

    created project activities (#1)

---
+++ app/src/main/java/com/mldt/puppypals/Activities/SignUP.java
@@ -0,0 +1,16 @@
+package com.mldt.puppypals.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mldt.puppypals.R;
+
+public class SignUP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
