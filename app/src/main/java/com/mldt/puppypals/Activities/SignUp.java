b013510ef32274cf1133de52686f3de2245eb1cd
Mandy Mason <mnmason86@gmail.com>
2022-10-17 20:49:37 -0400

    added code for user signup with Cognito

---
+++ app/src/main/java/com/mldt/puppypals/Activities/SignUp.java
@@ -0,0 +1,53 @@
+package com.mldt.puppypals.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+import com.mldt.puppypals.R;
+
+public class SignUp extends AppCompatActivity {
+
+    public static final String TAG = "SignUpActivity";
+    public static final String SIGNUP_EMAIL_TAG = "Signup_Email_Tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        setUpSignUpForm();
+    }
+
+    private void setUpSignUpForm(){
+
+        findViewById(R.id.signupActivitySignupButton).setOnClickListener(view -> {
+            String userEmail = ((EditText) findViewById(R.id.signupActivityEmailInput)).getText().toString();
+            String userPassword = ((EditText) findViewById(R.id.signupActivityPasswordInput)).getText().toString();
+
+            Amplify.Auth.signUp(userEmail,
+                    userPassword,
+                    AuthSignUpOptions.builder()
+                            .userAttribute(AuthUserAttributeKey.email(), userEmail)
+                            .build(),
+                    success -> {
+                        Log.i(TAG, "Signup successful! " + success);
+                        Intent goToVerifyActivity = new Intent(SignUp.this, Verify.class);
+                        goToVerifyActivity.putExtra(SIGNUP_EMAIL_TAG, userEmail);
+                        startActivity(goToVerifyActivity);
+                    },
+                    failure -> {
+                        Log.i(TAG, "Signup failed with email " + userEmail + " with message: " + failure);
+                        runOnUiThread(() -> Toast.makeText(SignUp.this, "Signup Failed", Toast.LENGTH_SHORT).show());
+                    }
+            );
+        });
+    }
+}
