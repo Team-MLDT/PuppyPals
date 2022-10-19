2a7a36a5e6541b73e5bff5c11468b96dafeb9801
Mandy Mason <mnmason86@gmail.com>
2022-10-18 20:36:35 -0400

    added code to retrieve Events assigned to a user from the database. Need to get session user to determine which Events to get. set up Recycler View for Events on User's profile

--- app/src/main/java/com/mldt/puppypals/Activities/OwnProfileSettings.java
+++ app/src/main/java/com/mldt/puppypals/Activities/OwnProfileSettings.java
@@ -1,16 +1,64 @@
 package com.mldt.puppypals.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Event;
+import com.amplifyframework.datastore.generated.model.User;
+import com.mldt.puppypals.Adapters.UpcomingEventsRecyclerViewAdapter;
 import com.mldt.puppypals.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OwnProfileSettings extends AppCompatActivity {
+    public static final String TAG = "OwnProfileSettingsActivity";
+
+    List<Event> eventList = null;
+    UpcomingEventsRecyclerViewAdapter adapter;
+    User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_own_profile_settings);
+
+        eventList = new ArrayList<>();
+        getEventsFromDB();
+        setUpEventsRecyclerView();
+    }
+
+    private void getEventsFromDB(){
+        Amplify.API.query(
+                ModelQuery.list(Event.class),
+                success -> {
+                    Log.i(TAG, "Read Events successfully!");
+                    eventList.clear();
+                    for(Event dbEvent : success.getData()){
+                        if(dbEvent.getHost().equals(currentUser)){
+                            //currentUser currently not assigned - should be set to the current session user
+                            eventList.add(dbEvent);
+                        }
+                    }
+                    runOnUiThread(() -> {
+                        adapter.notifyDataSetChanged();
+                    });
+                },
+                failure -> Log.i(TAG, "Did not read Events successfully " + failure)
+        );
+    }
+
+    private void setUpEventsRecyclerView(){
+        RecyclerView eventsRecyclerView = findViewById(R.id.ownProfileActivityEventsRV);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        eventsRecyclerView.setLayoutManager(layoutManager);
+        adapter = new UpcomingEventsRecyclerViewAdapter(eventList, this);
+        eventsRecyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/mldt/puppypals/Adapters/UpcomingEventsRecyclerViewAdapter.java
+++ app/src/main/java/com/mldt/puppypals/Adapters/UpcomingEventsRecyclerViewAdapter.java
@@ -1,14 +1,21 @@
 package com.mldt.puppypals.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.amplifyframework.core.model.temporal.Temporal;
 import com.amplifyframework.datastore.generated.model.Event;
+import com.mldt.puppypals.Activities.EventDetails;
 import com.mldt.puppypals.R;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -31,11 +38,29 @@ public class UpcomingEventsRecyclerViewAdapter extends RecyclerView.Adapter<Upco
 
     @Override
     public void onBindViewHolder(@NonNull UpcomingEventsRecyclerViewAdapter.UpcomingEventsViewHolder holder, int position) {
+        TextView upcomingEventsTitleTV = holder.itemView.findViewById(R.id.upcomingEventsFragmentTitle);
+        String eventDesc = eventList.get(position).getEventDescription();
+        upcomingEventsTitleTV.setText(eventDesc);
 
+        TextView upcomingEventsDateTV = holder.itemView.findViewById(R.id.upcomingEventsFragmentDate);
+        Temporal.DateTime eventDate = eventList.get(position).getEventDate();
+        upcomingEventsDateTV.setText(eventDate.toString());
+
+        View eventsViewHolder = holder.itemView;
+        eventsViewHolder.setOnClickListener(view -> {
+            Intent goToEventDetailPage = new Intent(callingActivity, EventDetails.class);
+            callingActivity.startActivity(goToEventDetailPage);
+        });
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public static class UpcomingEventsViewHolder extends RecyclerView.ViewHolder {
+        public UpcomingEventsViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
     }
 }
