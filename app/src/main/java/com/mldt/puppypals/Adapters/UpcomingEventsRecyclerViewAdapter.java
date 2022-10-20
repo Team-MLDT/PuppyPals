d4757e95b8d2c66051bc8be1465d83d14b7295ca
Lucy Gelderloos <lucygel@gmail.com>
2022-10-19 22:17:18 -0700

    Event location displays on map in EventDetails

--- app/src/main/java/com/mldt/puppypals/Adapters/UpcomingEventsRecyclerViewAdapter.java
+++ app/src/main/java/com/mldt/puppypals/Adapters/UpcomingEventsRecyclerViewAdapter.java
@@ -46,11 +46,16 @@ public class UpcomingEventsRecyclerViewAdapter extends RecyclerView.Adapter<Upco
         String eventDateTime = eventList.get(position).getEventDate() + ", " + eventList.get(position).getEventTime();
         upcomingEventsDateTV.setText(eventDateTime);
 
+        String eventLat = eventList.get(position).getLat();
+        String eventLon = eventList.get(position).getLon();
+
         View eventsViewHolder = holder.itemView;
         eventsViewHolder.setOnClickListener(view -> {
             Intent goToEventDetailPage = new Intent(callingActivity, EventDetails.class);
             goToEventDetailPage.putExtra(EventDetails.EVENT_DESCRIPTION_TAG,eventDesc);
             goToEventDetailPage.putExtra(EventDetails.EVENT_DATE_TIME_TAG,eventDateTime);
+            goToEventDetailPage.putExtra(EventDetails.EVENT_LAT_TAG,eventLat);
+            goToEventDetailPage.putExtra(EventDetails.EVENT_LON_TAG,eventLon);
             callingActivity.startActivity(goToEventDetailPage);
         });
     }
