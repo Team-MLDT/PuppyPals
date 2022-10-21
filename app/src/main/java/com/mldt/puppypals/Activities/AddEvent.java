8ed3ea743b31b8e9679e5fa91f2111daf465a6ed
Mandy Mason <mnmason86@gmail.com>
2022-10-20 20:41:21 -0400

    refactored location service

--- app/src/main/java/com/mldt/puppypals/Activities/AddEvent.java
+++ app/src/main/java/com/mldt/puppypals/Activities/AddEvent.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -21,47 +22,15 @@ import com.google.android.gms.tasks.OnTokenCanceledListener;
 import com.mldt.puppypals.R;
 
 public class AddEvent extends AppCompatActivity {
-    public static final String Tag = "Location";
-    // Create the location client
-    FusedLocationProviderClient fusedLocationClient;
+    public static final String TAG = "Location";
+    FusedLocationProviderClient fusedLocationClient = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        // Setup location Client & request permissions
-        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1); // hardcoded 1; can be anything between 1 - 65535
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
-        fusedLocationClient.flushLocations();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
-            @NonNull
-            @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
-            @Override
-            public boolean isCancellationRequested() {
-                return false;
-            }
-        }).addOnSuccessListener(location -> {
-            if(location == null){
-                Log.e(Tag, "Location callback was null");
-
-            }
-            Log.i(Tag, "Latitude: " + location.getLatitude());
-            Log.i(Tag, "Longitude: " + location.getLongitude());
+        setUpLocation();
 
             Event newEvent = Event.builder()
                     .eventDescription("Description")
@@ -74,11 +43,19 @@ public class AddEvent extends AppCompatActivity {
 
             Amplify.API.mutate(
                     ModelMutation.create(newEvent),
-                    success -> Log.i(Tag,"Add Event made Successfully"),
-                    failure -> Log.i(Tag, "failed to add event")
+                    success -> Log.i(TAG,"Add Event made Successfully"),
+                    failure -> Log.i(TAG, "failed to add event")
             );
-                Intent goToMainActivity = new Intent(AddEvent.this, MainActivity.class);
+    }
 
-        });
+    private void setUpLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Log.e(TAG, "No permissions for fine or coarse location");
+            return;
+        }
+        // Setup location Client & request permissions
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1); // hardcoded 1; can be anything between 1 - 65535
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        fusedLocationClient.flushLocations();
     }
 }
